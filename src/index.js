bf63bd3c82c8cc41e61cde72b2c9d4ed59bb727e
Tapesh <tapeshchahar2002@gmail.com>
2023-10-24 10:21:52 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -232,3 +232,16 @@ import ReactDOM from "react-dom";
 // <p>{myName()}</p>
 
 // </>,document.getElementById("root"));
+//---------------------------------------------------------------------------
+
+// ---------------------------REACT HOOKS ----------------------------------
+//1-- Hooks are introduced in the React 16.8 version
+//2-- It allows us to use state and other React features without writing a 
+// class. Hooks are the functions which "hook into" react state and lifecycle 
+// features from functional components.
+//3-- It does not work inside classes
+//4-- Hooks should always be used at the top level of the React.
+
+
+
+
