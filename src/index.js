0a2f9a9cd4e59db4383c6559805f323cefb9937d
Tapesh <tapeshchahar2002@gmail.com>
2023-10-31 15:01:30 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -245,6 +245,6 @@ import ReactDOM from "react-dom";
 //4-- Hooks should always be used at the top level of the React.
 // 5-- Hooks are very beneficial for the ease of the programmers.
 //
-
+//
 
 
